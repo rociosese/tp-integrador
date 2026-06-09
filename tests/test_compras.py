070d763b0007eccebc7922ef8edac6fdf4a5f217
rociosese <rociosese@hotmail.com>
2026-06-09 10:32:57 -0300

    Create test_compras.py

---
+++ tests/test_compras.py
@@ -0,0 +1,38 @@
+from src.compras import calcular_total, producto_mas_vendido
+
+def test_calcular_total():
+  compras = [
+    {"producto" : "arroz", "cantidad": 2, "precio": 1000}
+    {"producto" : "fideos", "cantidad": 3, "precio": 500}
+  ]
+
+  assert calcular_total(compras) == 3500
+
+def test_producto_mas_vendido():
+  compras = [
+    {"producto" : "arroz", "cantidad": 2, "precio": 1000}
+    {"producto" : "fideos", "cantidad": 5, "precio": 500}
+  ]
+
+  assert producto_mas_vendido(compras) == "fideos"
+
+def test_total_vacio():
+  compras = []
+
+  assert calcular_total(compras) == 0
+
+def test_un_solo_producto():
+  compras = [
+    {"producto" : "leche", "cantidad": 1, "precio": 1000}
+  ]
+
+  assert calcular_total(compras) == 1000
+
+def test_producto_mas_vendido_unico():
+  compras = [
+    {"producto" : "agua", "cantidad": 10, "precio": 100}
+  ]
+
+  assert producto_mas_vendido(compras) == "agua"
+
+
